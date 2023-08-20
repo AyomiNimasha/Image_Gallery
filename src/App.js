d26a74b23bc800de40f82918d9a17c2aa3328cd7
AyomiNimasha <ayominimasha@gmail.com>
2023-08-20 17:55:29 +0530

    commit1

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import flower2 from '../src/assets/images/flower2.jpeg';
+
+import './App.css';
+
+function App() {
+  return(
+    <div className='max-w-sm rounded overflow-hidden shadow-lg'>
+      <img src={flower2} alt='' className='w-full
+      '/>
+    </div>
+  )
+}
+
+export default App;
