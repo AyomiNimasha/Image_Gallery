25f16cd589ef7cc061a16e8950edfc5197e4cfda
AyomiNimasha <ayominimasha@gmail.com>
2023-08-22 21:04:33 +0530

    COMMIT3

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import React from 'react';
-import flower2 from '../src/assets/images/flower2.jpeg';
-
+import React, {useState,useEffect} from 'react';
+import './index.css';
 import './App.css';
-
+import { ImageCard } from './component/ImageCard';
+import { ImageSearch } from './component/ImageSearch';
 function App() {
+  const[images,setImages]=useState([]);
+  const[isLoading,setIsLoading]=useState(true);
+  const[term,setTerm]=useState('');
+
+  useEffect(()=>{
+    fetch(`https://pixabay.com/api/?key=38949323-234a4f2e04526fb2c0c198d47&q={term}&image_type=photo&pretty=true`)
+    .then(res=>res.json())
+    .then(data=>{
+      setImages(data.hits);
+      setIsLoading(false);
+    })
+    .catch(err=>console.log(err));
+  },[term]);
   return(
-    <div className='max-w-sm rounded overflow-hidden shadow-lg'>
-      <img src={flower2} alt='' className='w-full
-      '/>
-      <div className='px-6 py-4'>
-        <div className='font-bold text-purple-900 text-xl mb-2'>
-          Photo by Jhon Doe
-        </div>
-        <ul>
-          <li>
-            <strong>Views:</strong>
-            4000
-          </li>
-        </ul>
-      </div>
+    <div className='container mx-auto'>
+      <ImageSearch searchText={(text)=>setTerm(text)}/>
+      {!isLoading && images.length===0 && <h1 className='text-5xl text-center mx-auto mt-32'>No Images Found</h1>}
+      
+      {isLoading ? <h1 className='text-6xl text-center mx-auto mt-32'>Loading...</h1>:<div className='grid grid-cols-3 gap-4'>
+        {images.map(image=>(
+          <ImageCard key={image.id} image={image}/>
+        ))}
+      
+      </div>}
     </div>
+   
+      
   )
 }
 
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './assets/main.css';
 
+
 ReactDOM.render(
   <App />,
   document.getElementById('root')
